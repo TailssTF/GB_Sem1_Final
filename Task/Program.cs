f041e250239427f5ee134db22057a60ade109255
TailssTF <tailss5@mail.ru>
2023-02-12 23:07:57 +0700

    Added code

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,59 @@
 ﻿
+
+//метод считывания числа с клавиатуры
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+//метод вывода массива строк
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    foreach (string e in array) Console.Write($"{e} ");
+    Console.Write("]");
+}
+
+
+int n = GetNumber("Введите размер массива");
+Console.WriteLine("Введите элементы массива");
+string[] arr = new string[n];
+int count = 0;
+
+for (int i = 0; i < n; i++)
+{
+    arr[i] = Console.ReadLine();
+    if (arr[i].Length <= 3) count++;
+}
+
+Console.WriteLine("Исходный массив:");
+PrintArray(arr);
+
+string[] arr2 = new string[count];
+int k = 0;
+for (int i = 0; i < n; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        arr2[k] = arr[i];
+        k++;
+    }
+}
+
+Console.WriteLine("\nРезультат:");
+PrintArray(arr2);
